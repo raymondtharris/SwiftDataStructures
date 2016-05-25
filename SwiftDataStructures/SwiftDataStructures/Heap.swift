c07bd38f03419b05b99e0d65843c4cbdae68808a
Tim Harris <raymondtimothyharris@gmail.com>
2016-05-24 21:42:59 -0700

    More Heap Structure
    Added some more framing for the heap data structure.

--- SwiftDataStructures/SwiftDataStructures/Heap.swift
+++ SwiftDataStructures/SwiftDataStructures/Heap.swift
@@ -15,19 +15,58 @@ enum HeapType {
 class Heap<Element where Element : Comparable> {
     var heapType: HeapType
     var keys: [Element] = [Element]()
+    var heapsize: Int = 0
     
     init(newHeapType: HeapType) {
         heapType = newHeapType
     }
     
     func pop() -> Element? {
-        return nil
+        let toReturn = keys[0]
+        
+        return toReturn
     }
     
     func push(newKey: Element?) {
-        
+        if heapType == .maxHeap {
+            
+        } else {
+            
+        }
     }
     func peek() -> Element? {
+        if heapType == .maxHeap {
+            
+        } else {
+            
+        }
         return nil
     }
+    
+    
+    func maxHeapify(Index:Int) -> Void {
+        
+    }
+    
+    func minHeapify(Index: Int) -> Void {
+        
+    }
+    
+    func sort() {
+        
+    }
+    
 }
+
+func parent(index: Int) -> Int {
+    return index / 2
+}
+
+func left(index: Int) -> Int {
+    return 2 * index
+}
+
+func right(index: Int) -> Int {
+    return 2 * index + 1;
+}
+
